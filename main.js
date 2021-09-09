89a730705a463ae8af4e726d96b12c054b90d5b7
TT <83678908+B0BA-T3A-Coding@users.noreply.github.com>
2021-09-09 18:13:41 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
-var names = ["Family Book", "Dad", "Brother", "Me", "Mom"];
+var images = ["https://https://tse2.mm.bing.net/th?id=OIP.ml0Uu4nIp1s81eevnNJ9bwHaHa&pid=Api&P=0&w=300&h=300i.postimg.cc/MGn9GJXw/family.jpg", "https://tse4.mm.bing.net/th?id=OIP._ZnE5M0asQj_2ITthOc54gHaH5&pid=Api&P=0&w=300&h=300" , "https://tse2.mm.bing.net/th?id=OIP.FM-AgOyS2CuvqvGZfl5t1gHaQ3&pid=Api&P=0&w=300&h=300", "https://i.postimg.cc/JnL6wtrd/sisthttp://clipground.com/images/boy-clipart-9.jpger.jpg", "https://cliparting.com/wp-content/uploads/2018/03/cartoon-girl-2018-48.jpg"];
+var names = ["Family Book", "Dad", "Mom", "Sister(Me)", "Brother"];
 var i=0;
 function next(){
   document.getElementById("FI").src=image[i];
